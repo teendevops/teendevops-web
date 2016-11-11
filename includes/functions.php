ca9019b796325305e9caf71f39cacf087fcfb190
Arinerron <Arinerron@users.noreply.github.com>
2016-11-11 10:36:10 -0800

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -343,4 +343,28 @@ function isEmailValid($email) {
 function isUsernameValid($str) {
     return preg_match('/^[a-zA-Z0-9_]+$/',$str);
 }
+
+function showSimilar() {
+    if(isSignedIn()) {
+        echo "<center><h1>Meet other devs...<h1></center>";
+        echo "<div class=\"container\">
+  <div class=\"row\">";
+  
+        $settings = getSettings($_SESSION['id']);
+        $array = getUsersByLanguage($settings['languages']);
+        
+        foreach($array as $usr) {
+            echo "          <div class=\"col-sm-3\"><center>
+                                <img src=\"assets/user-icons/default.png\" id=\"icon-front\">
+                                <h3>" . $settings['languages'] . " Developer</h3>
+                            </center></div>";
+            echo "          <div class=\"col-sm-3\">
+                                <center><h2><a href=\"profile.php?id=" . $usr['id'] . "\">" . htmlspecialchars($usr['username']) . "</a></h2>
+                                This is a sample text description of the user. Later, when I join the `settings` and `users` tables, I'll be able to implement this and make it actually display the real description. For now, however, this pointless paragraph will remain riiiiiight here.
+                            </div></center>";
+        }
+        
+        echo "</div></div>";
+    }
+}
 ?>
