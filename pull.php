4de050c3186fb96053ff6486c2601090478b8afb
Duncan X. Simpson <virtualdxs@gmail.com>
2017-01-02 07:45:25 +0000

    Added script to pull latest version of site from repo

---
+++ pull.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>git pull</title>
+    </head>
+    <body style="background-color:black;">
+        <pre style="color:#0f0;">
+$ git pull -v
+<?php
+echo shell_exec('sudo -u duncan /usr/bin/git pull -v 2>&1');
+?>
+        </pre>
+    </body>
+</html>
