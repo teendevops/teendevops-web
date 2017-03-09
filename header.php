aedd4ea7b54bd21126129d4e9aaa10807c41f432
Arinerron <me@arinerron.com>
2017-03-08 20:35:19 -0800

    Mobile theming

--- header.php
+++ header.php
@@ -17,6 +17,12 @@
     <meta http-equiv="content-type" content="text/html"; charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#ec644b" />
+    <meta name="msapplication-navbutton-color" content="#ec644b" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <link rel="stylesheet" type="text/css" href="/css/styles.css">
     <link rel="stylesheet" href="/css/bootstrap.min.css">
 
