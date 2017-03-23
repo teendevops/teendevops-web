8b610e3dda58bd83e48def20f26e114beb4e58c6
Arinerron <me@arinerron.com>
2017-03-22 19:38:05 -0700

    update privacy policy

--- privacy.php
+++ privacy.php
@@ -4,7 +4,7 @@
     <body>
         <div class="container-fluid">
             <center><h2>teendevops.net Privacy Policy</h2>
-            <b>Disclaimer:</b> The privacy policy may change at any time without prior warning or notification. This page will always contain the most updated version, idenitified by the date at the bottom.<br><br>
+            The privacy policy may change at any time without prior warning or notification. This page will always contain the most updated version, idenitified by the date at the bottom.<br><br>
 
             <b>Do we store your information?</b><br>
             We store information about each user, such as usernames, email addresses, and user descriptions. Passwords are hashed and salted using <a href="https://en.wikipedia.org/wiki/Bcrypt">bcrypt</a>, and the plaintext version is never stored (except in RAM). Like most websites, we log the traffic on this site for statistical and security reasons. Information collected by logs include the IP address, URL, and time of an HTTP request.<br><br>
@@ -16,7 +16,7 @@
             In the event that the site or the database is compromise, a predefined procedure will be followed by the administrators. First, the site will go down for maintainance to prevent further exploitation. Then, the logs will be searched and the case will be investigated. When a patch is released (no later than 2 days), all users will be notified about the breach. Further actions will be taken, depending on the circumstances.<br><br>
 
             If you have any questions regarding this privacy policy, don't hesitate to contact us at <a href="mailto:info@teendevops.net">info@teendevops.net</a>.<br>
-            Last edited on March 21, 2017.
+            Last edited on March 23, 2017.
         </center></div>
 
         <?php include "footer.php"; ?>
