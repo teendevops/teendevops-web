8433a2bf543d3d944d00827f27fbb603bf3a1a5a
Arinerron <me@arinerron.com>
2017-07-13 23:18:03 -0700

    Fix spelling error
    `compromise` to `compromised`

--- privacy.php
+++ privacy.php
@@ -7,13 +7,13 @@
             The privacy policy may change at any time without prior warning or notification. This page will always contain the most updated version, idenitified by the date at the bottom.<br><br>
 
             <b>Do we store your information?</b><br>
-            We store information about each user, such as usernames, email addresses, and user descriptions. Passwords are hashed and salted using <a href="https://en.wikipedia.org/wiki/Bcrypt">bcrypt</a>, and the plaintext version is never stored (except in RAM). Like most websites, we log the traffic on this site for statistical and security reasons. Information collected by logs include the IP address, URL, and time of an HTTP request.<br><br>
+            We store information about each user, such as usernames, email addresses, and user descriptions. Passwords are hashed and salted using <a href="https://en.wikipedia.org/wiki/Bcrypt">bcrypt</a>, and the plaintext version is never stored (except possibly in RAM). Like most websites, we log the traffic on this site for statistical and security reasons. Information collected by logs include the IP address, URL, and time of an HTTP request.<br><br>
 
             <b>How do we use cookies?</b><br>
             We use cookies for identifying your session so you can use the services on this site. Cookies are not used for tracking.<br><br>
 
             <b>What happens if this site is compromised?</b><br>
-            In the event that the site or the database is compromise, a predefined procedure will be followed by the administrators. First, the site will go down for maintainance to prevent further exploitation. Then, the logs will be searched and the case will be investigated. When a patch is released (no later than 2 days), all users will be notified about the breach. Further actions will be taken, depending on the circumstances.<br><br>
+            In the event that the site or the database is compromised, a predefined procedure will be followed by the administrators. First, the site will go down for maintainance to prevent further exploitation. Then, the logs will be searched and the case will be investigated. When a patch is released (no later than 2 days), all users will be notified about the breach. Further actions will be taken, depending on the circumstances.<br><br>
 
             If you have any questions regarding this privacy policy, don't hesitate to contact us at <a href="mailto:info@teendevops.net">info@teendevops.net</a>.<br>
             Last edited on March 23, 2017.
